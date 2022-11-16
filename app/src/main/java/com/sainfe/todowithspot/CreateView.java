ac29f3c3c3d9517ee2c6c9e505fd7a53829fb331
seoeunbae <enyoupark@gmail.com>
2022-11-16 12:29:32 +0900

    feat : create-view의 < 버튼 클릭 시, today-view로 돌아가기

--- app/src/main/java/com/sainfe/todowithspot/CreateView.java
+++ app/src/main/java/com/sainfe/todowithspot/CreateView.java
@@ -2,7 +2,9 @@ package com.sainfe.todowithspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateView extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class CreateView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_view);
+
+        View backwardButton = findViewById(R.id.backward_button);
+        backwardButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent todayViewintent = new Intent(getApplicationContext(), TodayView.class);
+                startActivity(todayViewintent);
+            }
+        });
     }
 }
