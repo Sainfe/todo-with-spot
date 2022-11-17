c1ced6df1cd52e406bebcbce0c1ba06498a447c8
seoeunbae <enyoupark@gmail.com>
2022-11-17 14:23:56 +0900

    feat : today-view 의 todolist에 longclick작동 구현 진행중

--- app/src/main/java/com/sainfe/todowithspot/TodayView.java
+++ app/src/main/java/com/sainfe/todowithspot/TodayView.java
@@ -1,11 +1,13 @@
 package com.sainfe.todowithspot;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -49,10 +51,13 @@ public class TodayView extends AppCompatActivity {
 
             @Override
             public boolean onLongClick(View view) {
+                if(view == recyclerViewLayout){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(TodayView.this);
 
-                return false;
+                }
             }
         });
+
     }
 
 
