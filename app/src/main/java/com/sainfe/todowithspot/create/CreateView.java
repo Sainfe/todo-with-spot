1a00ede9905dc0608283bb0c94ce4ad44e185687
Hanna922 <hanna.urssu@gmail.com>
2022-11-19 19:57:33 +0900

    장소 설정 toggle 추가

--- app/src/main/java/com/sainfe/todowithspot/create/CreateView.java
+++ app/src/main/java/com/sainfe/todowithspot/create/CreateView.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 import com.sainfe.todowithspot.MainActivity;
 import com.sainfe.todowithspot.R;
@@ -23,6 +27,22 @@ public class CreateView extends AppCompatActivity {
             public void onClick(View view) {
                 Intent todayViewintent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(todayViewintent);
+            }
+        });
+
+        ToggleButton toggle = findViewById(R.id.toggle);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        RadioButton leaveButton = findViewById(R.id.leave_button);
+        RadioButton arriveButton = findViewById(R.id.arrive_button);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    leaveButton.setEnabled(true);
+                    arriveButton.setEnabled(true);
+                } else {
+                    leaveButton.setEnabled(false);
+                    arriveButton.setEnabled(false);
+                }
             }
         });
     }
