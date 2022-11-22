3af77911bffe99271867593b59555cbaa9e636e2
imstarbox <imstarbox.dev@gmail.com>
2022-11-22 15:07:41 +0900

    feat: ViewModel에 Firestore 쿼리 메소드 추가

--- app/src/main/java/com/sainfe/todowithspot/viewmodel/all/AllViewModel.java
+++ app/src/main/java/com/sainfe/todowithspot/viewmodel/all/AllViewModel.java
@@ -1,6 +1,42 @@
 package com.sainfe.todowithspot.viewmodel.all;
 
+import androidx.annotation.Nullable;
 import androidx.databinding.BaseObservable;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class AllViewModel extends BaseObservable {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    // TODO: sharedPreference 데이터 저장
+    String uid = "";
+
+    void reload() {
+        db.collection("users").document(uid).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    // TODO: Error
+                    return;
+                }
+                if (value != null && value.exists()) {
+                    // TODO: Success && exist value
+                } else {
+                    // TODO: Success && none value
+                }
+            }
+        });
+    }
+
+    void patchDoneTodo(DocumentSnapshot todo) {
+        boolean isDone = Boolean.TRUE.equals(todo.getBoolean("isDone"));
+        db.collection("users").document(uid).collection("todos").document(todo.getId()).update("isDone", !isDone);
+    }
+
+    void deleteTodo(DocumentSnapshot todo) {
+        db.collection("users").document(uid).collection("todos").document(todo.getId()).delete();
+    }
 }
