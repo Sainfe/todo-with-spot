60683b8d42d6bbd328dbb8e49004e5c529d2b241
imstarbox <imstarbox.dev@gmail.com>
2022-11-23 13:16:48 +0900

    feat: Firestore 데이터 실시간 연결

--- app/src/main/java/com/sainfe/todowithspot/view/today/TodayActivity.java
+++ app/src/main/java/com/sainfe/todowithspot/view/today/TodayActivity.java
@@ -2,7 +2,10 @@ package com.sainfe.todowithspot.view.today;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
@@ -23,8 +26,11 @@ import java.util.ArrayList;
 
 public class TodayActivity extends AppCompatActivity {
 
-    AlertDialog alertDialog;
     ActivityTodayBinding binding;
+    TodayViewModel viewModel = new TodayViewModel();
+    AlertDialog alertDialog;
+
+    ArrayList<Todo> todoList;
 
     @SuppressLint("ClickableViewAccessibility") // performClick()을 override 하라는 경고 음소거
     @Override
@@ -32,18 +38,25 @@ public class TodayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_today);
-        binding.setViewModel(new TodayViewModel());
+        binding.setViewModel(viewModel);
         binding.executePendingBindings();
 
+        viewModel.reload();
+        todoList = viewModel.getTodoList().getValue();
         bindList();
 
-        binding.createTodoListButton.setOnClickListener(new View.OnClickListener() {
-
+        viewModel.getTodoList().observe(this, new Observer<ArrayList<Todo>>() {
             @Override
-            public void onClick(View view) {
-                Intent createViewIntent = new Intent(getApplicationContext(), CreateActivity.class);
-                startActivity(createViewIntent);
+            public void onChanged(ArrayList<Todo> todos) {
+                todoList = todos;
+                bindList();
             }
+        });
+
+        // createButton 클릭 시 , create view 전환
+        binding.createTodoListButton.setOnClickListener(view -> {
+            Intent createIntent = new Intent(getApplicationContext(), CreateActivity.class);
+            startActivity(createIntent);
         });
 
         binding.layoutToday.setOnTouchListener(new OnSwipeTouchListener(this) {
@@ -66,41 +79,32 @@ public class TodayActivity extends AppCompatActivity {
     }
 
     public void bindList() {
-        final ArrayList<Todo> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            list.add(new Todo("text " + i, new Timestamp(10L, 10), false, false, null, 0, null));
-            // TODO : 데이테베이스에서 전체 조회로 구현 예정
-        }
-
         // 리사이클러뷰에 LinearLayoutManager 객체 지정.
-        binding.recycler1.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
-        TodayAdapter adapter = new TodayAdapter(list);
-        binding.recycler1.setAdapter(adapter);
+        TodayAdapter adapter = new TodayAdapter(todoList);
+        binding.recyclerView.setAdapter(adapter);
 
-        adapter.setOnItemLongClickListener(new TodayAdapter.OnItemLongClickEventListener() {
-            @Override
-            public void onItemLongClick(View view, int position) {
-                final Todo item = list.get(position);
+        adapter.setOnItemLongClickListener((view, position) -> {
+            final Todo item = todoList.get(position);
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(TodayActivity.this, R.style.AppCompatAlertDialog);
-                builder.setTitle(item.getContent()); // TODO : 뷰에 저장된 데이터 조회
-                builder.setMessage("TEST_MSG");
-                builder.setPositiveButton("확인", null);
-                builder.setNegativeButton("수정", new DialogInterface.OnClickListener() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(TodayActivity.this, R.style.AppCompatAlertDialog);
+            builder.setTitle(item.getContent()); // TODO : 뷰에 저장된 데이터 조회
+            builder.setMessage("TEST_MSG");
+            builder.setPositiveButton("확인", null);
+            builder.setNegativeButton("수정", new DialogInterface.OnClickListener() {
 
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //  TODO : EDIT 화면으로 넘어가기
-                        Intent intent = new Intent(getApplicationContext(), CreateActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //  TODO : EDIT 화면으로 넘어가기
+                    Intent intent = new Intent(getApplicationContext(), CreateActivity.class);
+                    startActivity(intent);
+                }
+            });
 
-                alertDialog = builder.create();
-                alertDialog.show();
-            }
+            alertDialog = builder.create();
+            alertDialog.show();
         });
     }
 }
